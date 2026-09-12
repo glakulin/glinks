5927f0dd51af75cc26b4dc81d36ae6ed7f2b0932
glakulin <glakulin@proton.me>
2026-09-12 13:50:27 -0700

    Update LinksPage.tsx

--- src/components/LinksPage.tsx
+++ src/components/LinksPage.tsx
@@ -12,10 +12,27 @@ type Links_Page_Props = {
   links?: readonly Links_Item[];
 };
 
+function shuffle<T>(array: readonly T[]): T[] {
+  const result = [...array];
+  let currentIndex = result.length;
+
+  while (currentIndex !== 0) {
+    const randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [result[currentIndex], result[randomIndex]] = [
+      result[randomIndex],
+      result[currentIndex],
+    ];
+  }
+
+  return result;
+}
+
 export function LinksPage({ Card, links = LINKS }: Links_Page_Props) {
   return (
     <>
-      {links.map((link) => (
+      {shuffle(links).map((link) => (
         <Card key={link.href} icon={link.icon} href={link.href}>
           {link.name}
         </Card>
